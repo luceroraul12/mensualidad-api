d1433d8685d6fd74d22bd6fdb42dd7629f70617e
Homitowen <pycgames@gmail.com>
2022-08-27 16:56:45 -0300

    por lo menos funciona el test de este repositorio

---
+++ src/test/java/ar/my/mensualidades/repositories/FacturaRepositoryTest.java
@@ -0,0 +1,41 @@
+package ar.my.mensualidades.repositories;
+
+import ar.my.mensualidades.models.Factura;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class FacturaRepositoryTest {
+
+    @Autowired
+    FacturaRepository facturaRepository;
+
+    @BeforeEach
+    void beforeAll() {
+
+        List<Factura> conjunto = new ArrayList<>();
+
+        conjunto.add(new Factura("Movistar"));
+        conjunto.add(new Factura("Carrefour"));
+        conjunto.add(new Factura("Megatone"));
+
+        facturaRepository.saveAll(conjunto);
+
+    }
+
+    @Test
+    void testCantidadCuardad(){
+        int resultado = facturaRepository.findAll().size();
+
+        assertEquals(3, resultado);
+
+    }
+}
