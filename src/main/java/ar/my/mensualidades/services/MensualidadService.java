e1410a53e7cc88882a0fe2edab360ee9134e2c4a
Homitowen <pycgames@gmail.com>
2022-08-27 17:55:29 -0300

    add interface del mensualidad con las firmas de los metodos del momento

---
+++ src/main/java/ar/my/mensualidades/services/MensualidadService.java
@@ -0,0 +1,24 @@
+package ar.my.mensualidades.services;
+
+import ar.my.mensualidades.models.Factura;
+import ar.my.mensualidades.models.Pago;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public interface MensualidadService {
+
+    List<Factura> getallFacturas();
+    List<Pago> getAllPagos();
+
+    /**
+     * Busca facturas que han pagado y no pagaron.
+     * @param fecha a la que se quiere comprobar. no debe ser null
+     * @return conujunto Cargado y No Cargado
+     */
+    List<Factura> getFacturasConySinCargaByFecha(LocalDate fecha);
+    List<Pago> getPagosByFecha(LocalDate fecha);
+
+}
