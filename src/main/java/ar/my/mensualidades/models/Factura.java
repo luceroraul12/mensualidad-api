d73a39c3ed897a3514f0251d645761b1311bedb8
Homitowen <pycgames@gmail.com>
2022-08-27 16:37:18 -0300

    add. entidad

---
+++ src/main/java/ar/my/mensualidades/models/Factura.java
@@ -0,0 +1,21 @@
+package ar.my.mensualidades.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "factura")
+public class Factura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
