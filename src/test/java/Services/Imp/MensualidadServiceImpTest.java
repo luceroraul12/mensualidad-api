39fe39e9d0a27933145c8fabe8a2d10131b61381
Homitowen <pycgames@gmail.com>
2022-08-26 23:29:44 -0300

    add. test para mensualidades

---
+++ src/test/java/Services/Imp/MensualidadServiceImpTest.java
@@ -0,0 +1,12 @@
+package Services.Imp;
+
+import Models.Pago;
+import Models.Subscripcion;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MensualidadServiceImpTest {
+
+}
