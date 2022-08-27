b27817b65efeef518421154cc1bb852942265aa0
Homitowen <pycgames@gmail.com>
2022-08-27 12:47:27 -0300

    add. get por fecha de pago

--- src/main/java/Repositories/PagoRepository.java
+++ src/main/java/Repositories/PagoRepository.java
@@ -4,8 +4,16 @@ import Models.Pago;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PagoRepository extends JpaRepository<Pago, UUID> {
+    List<Pago> findByFechaPago(LocalDate fechaPago);
+
+
+
+
+
 }
