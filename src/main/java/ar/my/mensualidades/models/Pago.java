9b6eb51885a7a1f4ccc168062872a870bb5380af
Homitowen <pycgames@gmail.com>
2022-08-27 17:01:10 -0300

    add nueva entidad

---
+++ src/main/java/ar/my/mensualidades/models/Pago.java
@@ -0,0 +1,32 @@
+package ar.my.mensualidades.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "pago")
+public class Pago {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column
+    private Double pago;
+
+    @Column
+    private LocalDate fechaDePago;
+
+    @ManyToOne
+    @JoinColumn(name = "factura_id")
+    private Factura factura;
+
+    public Pago(Double pago, LocalDate fechaDePago, Factura factura) {
+        this.pago = pago;
+        this.fechaDePago = fechaDePago;
+        this.factura = factura;
+    }
+}
