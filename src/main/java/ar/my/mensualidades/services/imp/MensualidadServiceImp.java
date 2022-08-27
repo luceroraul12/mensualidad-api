a0da03707ba0798489085844526fcadb0b7b771f
Homitowen <pycgames@gmail.com>
2022-08-27 18:16:29 -0300

    implementación del metodo

--- src/main/java/ar/my/mensualidades/services/imp/MensualidadServiceImp.java
+++ src/main/java/ar/my/mensualidades/services/imp/MensualidadServiceImp.java
@@ -2,29 +2,55 @@ package ar.my.mensualidades.services.imp;
 
 import ar.my.mensualidades.models.Factura;
 import ar.my.mensualidades.models.Pago;
+import ar.my.mensualidades.repositories.FacturaRepository;
+import ar.my.mensualidades.repositories.PagoRepository;
 import ar.my.mensualidades.services.MensualidadService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MensualidadServiceImp implements MensualidadService {
+
+    @Autowired
+    FacturaRepository facturaRepository;
+    @Autowired
+    PagoRepository pagoRepository;
+
     @Override
     public List<Factura> getallFacturas() {
-        return null;
+        return facturaRepository.findAll();
     }
 
     @Override
     public List<Pago> getAllPagos() {
-        return null;
+        return pagoRepository.findAll();
     }
 
     @Override
-    public List<Factura> getFacturasConySinCargaByFecha(LocalDate fecha) {
+    public List<Factura> getfacturasCargadasFaltantesByFecha(LocalDate fecha) {
+        List<Factura> facturasTotales = facturaRepository.findAll();
+        List<Factura> facturasObtenidasDePagos = new ArrayList<>();
+
+        List<Factura> facturasCargadas;
+        List<Factura> facturasFaltantes;
+
+        pagoRepository.findByFechaDePago(fecha).stream().map(pago -> facturasObtenidasDePagos.add(pago.getFactura()));
+
+
+        facturasCargadas = facturasObtenidasDePagos;
+
+        facturasFaltantes = facturasTotales;
+        facturasFaltantes.removeAll(facturasCargadas);
+
+
+
         return null;
     }
 
     @Override
     public List<Pago> getPagosByFecha(LocalDate fecha) {
-        return null;
+        return pagoRepository.findByFechaDePago(fecha);
     }
 }
