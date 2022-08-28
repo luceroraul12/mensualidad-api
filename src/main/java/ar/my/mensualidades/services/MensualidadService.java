809e7fc0920fa62633dd3c39581d6ae43922ace8
Homitowen <pycgames@gmail.com>
2022-08-27 22:31:48 -0300

    update: tipo de respuesta

--- src/main/java/ar/my/mensualidades/services/MensualidadService.java
+++ src/main/java/ar/my/mensualidades/services/MensualidadService.java
@@ -1,5 +1,6 @@
 package ar.my.mensualidades.services;
 
+import ar.my.mensualidades.response.MensualidadResponse;
 import ar.my.mensualidades.models.Factura;
 import ar.my.mensualidades.models.Pago;
 import org.springframework.stereotype.Service;
@@ -16,8 +17,8 @@ public interface MensualidadService {
     /**
      * Busca facturas que han pagado y no pagaron.
      * @param fecha a la que se quiere comprobar. no debe ser null
-     * @return conujunto Cargado y No Cargado. 0 = Cargado, 1 = Faltante.
+     * @return conujunto Cargado y No Cargado. 0 = Cargado, 1 = Faltante, 2 = CostoTotalPagado
      */
-    List<List<Factura>> getfacturasCargadasFaltantesByFecha(LocalDate fecha);
+    MensualidadResponse getfacturasCargadasFaltantesByFecha(LocalDate fecha);
 
 }
