018c286a63c154ca79e191cdd4a103b51ebb605b
Lucero Raul <luceroraul12@gmail.com>
2022-08-28 10:13:54 -0300

    add atributos y rename

--- src/main/java/ar/my/mensualidades/response/MensualidadResponse.java
+++ src/main/java/ar/my/mensualidades/response/MensualidadResponse.java
@@ -1,15 +1,18 @@
 package ar.my.mensualidades.response;
 
 import ar.my.mensualidades.models.Factura;
+import ar.my.mensualidades.models.Pago;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class MensualidadResponse {
-
+    private List<Factura> facturasCreadas;
     private List<Factura> facturasCargadas;
     private List<Factura> facturasFaltantes;
+    private List<Pago> pagosCreados;
+    private List<Pago> pagosCargados;
     private Double costoTotalPagado;
 
 }
