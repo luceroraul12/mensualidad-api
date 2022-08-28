3fc02d2f0b52ffeef44c790774ffa7b693fea9b4
Homitowen <pycgames@gmail.com>
2022-08-27 22:31:14 -0300

    add respuesta completa

--- src/test/java/ar/my/mensualidades/services/imp/MensualidadServiceImpTest.java
+++ src/test/java/ar/my/mensualidades/services/imp/MensualidadServiceImpTest.java
@@ -4,12 +4,11 @@ import ar.my.mensualidades.models.Factura;
 import ar.my.mensualidades.models.Pago;
 import ar.my.mensualidades.repositories.FacturaRepository;
 import ar.my.mensualidades.repositories.PagoRepository;
+import ar.my.mensualidades.response.MensualidadResponse;
 import ar.my.mensualidades.services.MensualidadService;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -37,17 +36,17 @@ class MensualidadServiceImpTest {
 
         when(facturaRepositoryMock.findAll()).thenReturn(conjuntoFactura);
 
-        conjuntoPago.add(new Pago(234.23, LocalDate.now(), conjuntoFactura.get(0)));
-        conjuntoPago.add(new Pago(12323.23, LocalDate.now(), conjuntoFactura.get(1)));
+        conjuntoPago.add(new Pago(200.20, LocalDate.now(), conjuntoFactura.get(0)));
+        conjuntoPago.add(new Pago(100.30, LocalDate.now(), conjuntoFactura.get(1)));
 
-        when(pagoRepositoryMock.buscarPorMesyAnio(LocalDate.now())).thenReturn(conjuntoPago);
+        when(pagoRepositoryMock.buscarPorMesyAño(LocalDate.now())).thenReturn(conjuntoPago);
         when(pagoRepositoryMock.findAll()).thenReturn(conjuntoPago);
 
     }
 
     @Test
     void getfacturasCargadasFaltantesByFecha() {
-        List<List<Factura>> resultadoObtenido = mensualidadService.getfacturasCargadasFaltantesByFecha(LocalDate.now());
+        MensualidadResponse resultadoObtenido = mensualidadService.getfacturasCargadasFaltantesByFecha(LocalDate.now());
         List<Factura> conjuntoCargadoEsperado = new ArrayList<>();
         List<Factura> conjuntoFaltanteEsperado = new ArrayList<>();
 
@@ -56,7 +55,8 @@ class MensualidadServiceImpTest {
         conjuntoFaltanteEsperado.add(new Factura("Supercanal"));
 
 
-        assertArrayEquals(conjuntoCargadoEsperado.toArray(), resultadoObtenido.get(0).toArray());
-        assertArrayEquals(conjuntoFaltanteEsperado.toArray(), resultadoObtenido.get(1).toArray());
+        assertArrayEquals(conjuntoCargadoEsperado.toArray(), resultadoObtenido.getFacturasCargadas().toArray());
+        assertArrayEquals(conjuntoFaltanteEsperado.toArray(), resultadoObtenido.getFacturasFaltantes().toArray());
+        assertEquals(300.50, resultadoObtenido.getCostoTotalPagado());
     }
 }
