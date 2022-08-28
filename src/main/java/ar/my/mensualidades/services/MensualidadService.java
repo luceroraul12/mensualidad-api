08144bb41623379255771ffaf015655441c838ff
Lucero Raul <luceroraul12@gmail.com>
2022-08-28 11:04:54 -0300

    rename

--- src/main/java/ar/my/mensualidades/services/MensualidadService.java
+++ src/main/java/ar/my/mensualidades/services/MensualidadService.java
@@ -1,24 +1,21 @@
 package ar.my.mensualidades.services;
 
 import ar.my.mensualidades.response.MensualidadResponse;
-import ar.my.mensualidades.models.Factura;
-import ar.my.mensualidades.models.Pago;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Service
 public interface MensualidadService {
 
-    List<Factura> getallFacturas();
-    List<Pago> getAllPagos();
+    MensualidadResponse getallFacturas();
+    MensualidadResponse getAllPagos();
 
     /**
      * Busca facturas que han pagado y no pagaron.
      * @param fecha a la que se quiere comprobar. no debe ser null
      * @return conujunto Cargado y No Cargado. 0 = Cargado, 1 = Faltante, 2 = CostoTotalPagado
      */
-    MensualidadResponse getfacturasCargadasFaltantesByFecha(LocalDate fecha);
+    MensualidadResponse getResumenMensual(LocalDate fecha);
 
 }
