0dfeab27d4aaed356f273e1c6b917f2ab18f065d
Homitowen <pycgames@gmail.com>
2022-08-28 01:30:42 -0300

    add metodo metodo para obtener todo de un mes específico

--- src/main/java/ar/my/mensualidades/controllers/MensualidadController.java
+++ src/main/java/ar/my/mensualidades/controllers/MensualidadController.java
@@ -2,11 +2,16 @@ package ar.my.mensualidades.controllers;
 
 import ar.my.mensualidades.models.Factura;
 import ar.my.mensualidades.models.Pago;
+import ar.my.mensualidades.response.MensualidadResponse;
 import ar.my.mensualidades.services.MensualidadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -23,6 +28,12 @@ public class MensualidadController {
     @GetMapping("/pagos")
     public List<Pago> getAllPagos(){
         return mensualidadService.getAllPagos();
+    }
+
+    @GetMapping("/mensual")
+    public MensualidadResponse getResultadoMesElegido(@RequestParam("fecha") String fecha){
+
+        return mensualidadService.getfacturasCargadasFaltantesByFecha(LocalDate.parse(fecha));
     }
 
 
