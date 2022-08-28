ed479833674de8dea8af4746a9858a05d9146b44
Lucero Raul <luceroraul12@gmail.com>
2022-08-28 11:07:04 -0300

    add. constructor para añadirle la dependencia del servicio y poder implementar mockito

--- src/main/java/ar/my/mensualidades/controllers/MensualidadController.java
+++ src/main/java/ar/my/mensualidades/controllers/MensualidadController.java
@@ -1,18 +1,13 @@
 package ar.my.mensualidades.controllers;
 
-import ar.my.mensualidades.models.Factura;
-import ar.my.mensualidades.models.Pago;
 import ar.my.mensualidades.response.MensualidadResponse;
 import ar.my.mensualidades.services.MensualidadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 public class MensualidadController {
@@ -20,20 +15,23 @@ public class MensualidadController {
     @Autowired
     MensualidadService mensualidadService;
 
+    public MensualidadController(MensualidadService mensualidadService) {
+        this.mensualidadService = mensualidadService;
+    }
+
     @GetMapping("/facturas")
-    public List<Factura> getAllFacturas(){
+    public MensualidadResponse getAllFacturas(){
         return mensualidadService.getallFacturas();
     }
 
     @GetMapping("/pagos")
-    public List<Pago> getAllPagos(){
+    public MensualidadResponse getAllPagos(){
         return mensualidadService.getAllPagos();
     }
 
     @GetMapping("/mensual")
     public MensualidadResponse getResultadoMesElegido(@RequestParam("fecha") String fecha){
-
-        return mensualidadService.getfacturasCargadasFaltantesByFecha(LocalDate.parse(fecha));
+        return mensualidadService.getResumenMensual(LocalDate.parse(fecha));
     }
 
 
