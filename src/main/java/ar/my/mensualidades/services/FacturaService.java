5b2a1d677fd9246cadf2f94f93d2e26e1307fe56
Lucero Raul <luceroraul12@gmail.com>
2022-11-06 17:50:03 -0300

    add imp services

---
+++ src/main/java/ar/my/mensualidades/services/FacturaService.java
@@ -0,0 +1,7 @@
+package ar.my.mensualidades.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FacturaService extends MensualidadAbstractService {
+}
---
+++ src/main/java/ar/my/mensualidades/services/PagoService.java
@@ -0,0 +1,8 @@
+package ar.my.mensualidades.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PagoService extends MensualidadAbstractService{
+
+}
