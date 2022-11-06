b2fb590bcd15410647447c3cbc9459e3a5dd0a6e
Lucero Raul <luceroraul12@gmail.com>
2022-11-06 17:34:03 -0300

    add DI jpa

--- src/main/java/ar/my/mensualidades/services/MensualidadAbstractService.java
+++ src/main/java/ar/my/mensualidades/services/MensualidadAbstractService.java
@@ -1,8 +1,15 @@
 package ar.my.mensualidades.services;
 
+import ar.my.mensualidades.models.ModeloMensualidad;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public abstract class MensualidadAbstractService {
+
+    @Autowired
+    JpaRepository<ModeloMensualidad, Long> repository;
+
+
 
 
 
