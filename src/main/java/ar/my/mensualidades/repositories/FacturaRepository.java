6b2dc237940f0c2c2f215a0b8ac31016d3938067
Lucero Raul <luceroraul12@gmail.com>
2022-11-06 18:22:30 -0300

    create config bean

--- src/main/java/ar/my/mensualidades/repositories/FacturaRepository.java
+++ src/main/java/ar/my/mensualidades/repositories/FacturaRepository.java
@@ -2,6 +2,8 @@ package ar.my.mensualidades.repositories;
 
 import ar.my.mensualidades.models.Factura;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FacturaRepository extends JpaRepository<Factura, Long> {
 }
--- src/main/java/ar/my/mensualidades/repositories/PagoRepository.java
+++ src/main/java/ar/my/mensualidades/repositories/PagoRepository.java
@@ -4,20 +4,12 @@ import ar.my.mensualidades.models.Pago;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
-
+@Repository
 public interface PagoRepository extends JpaRepository<Pago, Long> {
-    /**
-     * Metodo para obtener los pagos que tengan el mismo mes y año de la fecha ingresada.
-     * @param fechaDePago no debe ser null
-     * @return conjunto que cumple
-     */
-    @Query("select p from Pago p where year(p.fechaDePago) = year(:fechaDePago)" +
-            "and month(p.fechaDePago) = month(:fechaDePago)")
-    List<Pago> buscarPorMesyAño(@Param("fechaDePago") LocalDate fechaDePago);
-
 
 
 }
