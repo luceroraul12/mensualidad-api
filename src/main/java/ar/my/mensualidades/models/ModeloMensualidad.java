114347d83f9a0db6144c2035b9241c2ee3c86ec6
Lucero Raul <luceroraul12@gmail.com>
2022-11-06 17:45:07 -0300

    add getter lombook

--- src/main/java/ar/my/mensualidades/models/ModeloMensualidad.java
+++ src/main/java/ar/my/mensualidades/models/ModeloMensualidad.java
@@ -1,10 +1,14 @@
 package ar.my.mensualidades.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Getter
 public abstract class ModeloMensualidad {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
