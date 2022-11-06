d07bbdd113f21f17209b59c8ce16afebb7149a1d
Lucero Raul <luceroraul12@gmail.com>
2022-11-06 16:58:47 -0300

    ??

--- src/main/java/ar/my/mensualidades/controllers/MensualidadController.java
+++ src/main/java/ar/my/mensualidades/controllers/MensualidadController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping("api")
 public class MensualidadController {
 
     @Autowired MensualidadService mensualidadService;
