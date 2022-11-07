df235179389d17291d2f27e77bcd151d5c645b43
Lucero Raul <luceroraul12@gmail.com>
2022-11-07 07:55:07 -0300

    delete cosas que no usan

--- src/main/java/ar/my/mensualidades/services/FacturaService.java
+++ src/main/java/ar/my/mensualidades/services/FacturaService.java
@@ -1,9 +1,6 @@
 package ar.my.mensualidades.services;
 
 import ar.my.mensualidades.models.Factura;
-import ar.my.mensualidades.models.ModeloMensualidad;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
