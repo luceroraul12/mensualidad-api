f2b89d95a5f40eca609a1eafaeea8f26969470f9
Lucero Raul <luceroraul12@gmail.com>
2022-11-08 12:42:01 -0300

    add postmapping resumen pago

--- src/main/java/ar/my/mensualidades/controllers/PagoController.java
+++ src/main/java/ar/my/mensualidades/controllers/PagoController.java
@@ -1,12 +1,25 @@
 package ar.my.mensualidades.controllers;
 
 import ar.my.mensualidades.models.Pago;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ar.my.mensualidades.repositories.PagoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("pagos")
 @CrossOrigin
 public class PagoController extends CrudAbstractController<Pago>{
+
+    @Autowired
+    PagoRepository pagoRepository;
+
+    @PostMapping("resumen")
+    public List<Pago> obtenerResumen(
+            @RequestParam("mes") Integer mes,
+            @RequestParam("anio") Integer anio){
+        return pagoRepository.obtenerPagosResumenMesyAnio(mes, anio);
+    }
 }
