01cea2285571d74b6be576c607f327d49b499388
Lucero Raul <luceroraul12@gmail.com>
2022-12-31 17:27:40 -0300

    mutando test

--- src/test/java/ar/my/mensualidades/services/ResumenServiceTest.java
+++ src/test/java/ar/my/mensualidades/services/ResumenServiceTest.java
@@ -9,10 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,14 +28,18 @@ class ResumenServiceTest {
                 new Factura("Tarjeta", null, false),
                 new Factura("Agua", null, true),
                 new Factura("Municipalidad", null, false),
-                new Factura("Telefono", null, false)
+                new Factura("Telefono", null, false),
+                new Factura("Panaderia", null, true)
+
         );
 
         pagos = Arrays.asList(
                 new Pago(23.24, null, null, facturas.get(0)),
+                new Pago(23.24, null, null, facturas.get(1)),
                 new Pago(21325.2, null, null, facturas.get(2)),
                 new Pago(1235.3, null, null, facturas.get(3)),
-                new Pago(153.365, null, null, facturas.get(4))
+                new Pago(153.365, null, null, facturas.get(4)),
+                new Pago(153.365, null, null, facturas.get(5))
         );
 
         Collections.shuffle(pagos);
@@ -46,18 +47,21 @@ class ResumenServiceTest {
     @Test
     void generarResumen() {
         ResumenDto resultado = service.generarResumen(pagos, facturas);
-        assertEquals(4, resultado.getFacturasPagadas().size());
+        assertEquals(6, resultado.getFacturasPagadas().size());
         assertEquals(3, resultado.getFacturasImpagas().size());
 
         List<String> facturasImpagas = resultado.getFacturasImpagas().stream()
-                        .map(Factura::getNombre)
-                        .toList();
+                .sorted(Comparator.comparing(Factura::getNombre))
+                .map(Factura::getNombre)
+                .toList();
 
-        assertTrue(facturasImpagas.contains("Tarjeta"));
         assertTrue(facturasImpagas.contains("Supermercado"));
         assertTrue(facturasImpagas.contains("Agua"));
+        assertTrue(facturasImpagas.contains("Panaderia"));
 
-        assertEquals("Tarjeta", facturasImpagas.get(0));
+        assertEquals("Agua",facturasImpagas.get(0));
+        assertEquals("Panaderia",facturasImpagas.get(1));
+        assertEquals("Supermercado",facturasImpagas.get(2));
 
     }
 }
