1241ff5a1f959fca423e31a728d122f488389f34
Lucero Raul <luceroraul12@gmail.com>
2023-01-08 10:50:51 -0300

    add condicionales de porcentualidad

--- src/main/java/ar/my/mensualidades/services/PorcentualidadService.java
+++ src/main/java/ar/my/mensualidades/services/PorcentualidadService.java
@@ -1,8 +1,46 @@
 package ar.my.mensualidades.services;
 
 import ar.my.mensualidades.models.Porcentualidad;
+import ar.my.mensualidades.repositories.PorcentualidadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PorcentualidadService extends MensualidadAbstractService<Porcentualidad>{
+
+    @Autowired
+    PorcentualidadRepository porcentualidadRepository;
+
+    @Override
+    public Porcentualidad crear(Porcentualidad modeloMensualidad) throws Exception {
+        if (cumplePorcentualidad(modeloMensualidad)){
+            return super.crear(modeloMensualidad);
+        } else {
+            throw new Exception("el porcentaje es superior a 100");
+        }
+    }
+
+    @Override
+    public Porcentualidad modificar(Porcentualidad modeloMensualidad) throws Exception {
+        if (cumplePorcentualidad(modeloMensualidad)){
+            return super.modificar(modeloMensualidad);
+        } else {
+            throw new Exception("el porcentaje es superior a 100");
+        }
+    }
+
+    public boolean cumplePorcentualidad(Porcentualidad porcentualidad){
+        List<Porcentualidad> porcentualidadAlmacenada = porcentualidadRepository.obtenerCoincidenciasPorUsuarioyFactura(
+                porcentualidad.getUsuario().getId(),
+                porcentualidad.getFactura().getId()
+        );
+        Double porcentajeTotalAlmacenado = porcentualidadAlmacenada.stream()
+                .map(Porcentualidad::getPorcentaje)
+                .reduce(Double::sum)
+                .get();
+
+        return porcentajeTotalAlmacenado <= 100.0;
+    }
 }
