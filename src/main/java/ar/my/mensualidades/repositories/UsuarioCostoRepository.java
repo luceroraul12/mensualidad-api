7cfc413bbdeb7fa787abdfabb11ed1b1fe67822a
Lucero Raul <luceroraul12@gmail.com>
2023-01-22 17:20:16 -0300

    delete clases que adicione, prefiero comenzarlas de nuevo para que se adapten a la base de datos

--- src/main/java/ar/my/mensualidades/repositories/UsuarioCostoRepository.java
+++
@@ -1,7 +0,0 @@
-package ar.my.mensualidades.repositories;
-
-import ar.my.mensualidades.models.UsuarioCosto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioCostoRepository extends JpaRepository<UsuarioCosto, Long> {
-}
--- src/main/java/ar/my/mensualidades/services/UsuarioCostoService.java
+++
@@ -1,8 +0,0 @@
-package ar.my.mensualidades.services;
-
-import ar.my.mensualidades.models.UsuarioCosto;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UsuarioCostoService extends MensualidadAbstractService<UsuarioCosto>{
-}
