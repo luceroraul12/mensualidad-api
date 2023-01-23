743a247f85a51063533606e2a014a79246889ac4
Lucero Raul <luceroraul12@gmail.com>
2023-01-23 08:20:43 -0300

    converter porcentaje usuarios facturas terminado

--- src/main/java/ar/my/mensualidades/models/converters/PorcentajeUsuariosFacturasConverter.java
+++ src/main/java/ar/my/mensualidades/models/converters/PorcentajeUsuariosFacturasConverter.java
@@ -1,4 +1,33 @@
 package ar.my.mensualidades.models.converters;
 
-public class PorcentajeUsuariosFacturasConverter {
+import ar.my.mensualidades.dto.PorcentajeUsuariosFacturasDto;
+import ar.my.mensualidades.models.Factura;
+import ar.my.mensualidades.models.PorcentajeUsuariosFacturas;
+import ar.my.mensualidades.models.Usuario;
+
+public class PorcentajeUsuariosFacturasConverter extends Converter<PorcentajeUsuariosFacturas, PorcentajeUsuariosFacturasDto>{
+    @Override
+    PorcentajeUsuariosFacturasDto toDto(PorcentajeUsuariosFacturas porcentajeUsuariosFacturas) {
+        return new PorcentajeUsuariosFacturasDto(
+                porcentajeUsuariosFacturas.getId(),
+                porcentajeUsuariosFacturas.getFactura().getId(),
+                porcentajeUsuariosFacturas.getUsuario().getId(),
+                porcentajeUsuariosFacturas.getPorcentaje()
+        );
+    }
+
+    @Override
+    PorcentajeUsuariosFacturas toEntidad(PorcentajeUsuariosFacturasDto porcentajeUsuariosFacturasDto) {
+        PorcentajeUsuariosFacturas entidad = new PorcentajeUsuariosFacturas();
+        Usuario usuario = new Usuario();
+        Factura factura = new Factura();
+        usuario.setId(porcentajeUsuariosFacturasDto.getIdUsuario());
+        factura.setId(porcentajeUsuariosFacturasDto.getIdFactura());
+
+        entidad.setId(porcentajeUsuariosFacturasDto.getId());
+        entidad.setUsuario(usuario);
+        entidad.setFactura(factura);
+        entidad.setPorcentaje(porcentajeUsuariosFacturasDto.getPorcentaje());
+        return entidad;
+    }
 }
