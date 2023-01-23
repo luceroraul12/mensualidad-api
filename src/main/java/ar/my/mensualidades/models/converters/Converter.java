e9f46ae5ef2e106b54091e1ed2fd849cda6b9f31
Lucero Raul <luceroraul12@gmail.com>
2023-01-22 22:07:53 -0300

    add abstract converter

---
+++ src/main/java/ar/my/mensualidades/models/converters/Converter.java
@@ -0,0 +1,15 @@
+package ar.my.mensualidades.models.converters;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class Converter<Entidad, EntidadDto> {
+    abstract EntidadDto toDto(Entidad entidad);
+    abstract Entidad toEntidad(EntidadDto entidad);
+    public List<Entidad> toEntidadList(List<EntidadDto> dtos){
+        return dtos.stream().map(this::toEntidad).collect(Collectors.toList());
+    }
+    public List<EntidadDto> toDtoList(List<Entidad> entidades){
+        return entidades.stream().map(this::toDto).collect(Collectors.toList());
+    }
+}
