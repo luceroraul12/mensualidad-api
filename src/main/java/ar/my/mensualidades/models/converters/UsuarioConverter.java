e934d826041de0b88e79b6085d1584fd2efd1a30
Lucero Raul <luceroraul12@gmail.com>
2023-01-23 08:15:13 -0300

    converter usuarios completado

--- src/main/java/ar/my/mensualidades/models/converters/UsuarioConverter.java
+++ src/main/java/ar/my/mensualidades/models/converters/UsuarioConverter.java
@@ -1,4 +1,19 @@
 package ar.my.mensualidades.models.converters;
 
-public class UsuarioConverter {
+import ar.my.mensualidades.dto.UsuarioDto;
+import ar.my.mensualidades.models.Usuario;
+
+public class UsuarioConverter extends Converter<Usuario, UsuarioDto>{
+    @Override
+    UsuarioDto toDto(Usuario usuario) {
+        return new UsuarioDto(usuario.getId(), usuario.getUsuario());
+    }
+
+    @Override
+    Usuario toEntidad(UsuarioDto usuarioDto) {
+        Usuario usuario = new Usuario();
+        usuario.setId(usuarioDto.getId());
+        usuario.setUsuario(usuarioDto.getUsuario());
+        return usuario;
+    }
 }
