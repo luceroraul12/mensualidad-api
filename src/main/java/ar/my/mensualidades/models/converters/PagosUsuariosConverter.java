cbf9fd41e585a73ecdc16537a607f21b337a39c6
Lucero Raul <luceroraul12@gmail.com>
2023-01-23 08:13:38 -0300

    converter pagos usuarios completado

---
+++ src/main/java/ar/my/mensualidades/models/converters/PagosUsuariosConverter.java
@@ -0,0 +1,33 @@
+package ar.my.mensualidades.models.converters;
+
+import ar.my.mensualidades.dto.PagosUsuariosDto;
+import ar.my.mensualidades.models.Factura;
+import ar.my.mensualidades.models.Pago;
+import ar.my.mensualidades.models.PagosUsuarios;
+import ar.my.mensualidades.models.Usuario;
+
+public class PagosUsuariosConverter extends Converter<PagosUsuarios, PagosUsuariosDto>{
+    @Override
+    PagosUsuariosDto toDto(PagosUsuarios pagosUsuarios) {
+        return new PagosUsuariosDto(
+                pagosUsuarios.getId(),
+                pagosUsuarios.getUsuario().getId(),
+                pagosUsuarios.getPago().getId(),
+                pagosUsuarios.getPagoFraccionado());
+    }
+
+    @Override
+    PagosUsuarios toEntidad(PagosUsuariosDto pagosUsuariosDto) {
+        PagosUsuarios pagosUsuarios = new PagosUsuarios();
+        Pago pago = new Pago();
+        Usuario usuario = new Usuario();
+        usuario.setId(pagosUsuariosDto.getIdUsuario());
+        pago.setId(pagosUsuariosDto.getIdPago());
+
+        pagosUsuarios.setId(pagosUsuariosDto.getId());
+        pagosUsuarios.setUsuario(usuario);
+        pagosUsuarios.setPago(pago);
+        pagosUsuarios.setPagoFraccionado(pagosUsuariosDto.getPagoFraccionado());
+        return pagosUsuarios;
+    }
+}
